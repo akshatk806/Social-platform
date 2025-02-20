7dd434b7f28943dab78755e36e530b03509e7f25
akshatkumar <akshatk806@gmail.com>
2025-02-20 22:47:11 +0530

    handling dependencies for component tests

--- src/app/user/user-list/user-list.component.spec.ts
+++ src/app/user/user-list/user-list.component.spec.ts
@@ -1,19 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { UserService } from '../user.service';
+import { of } from 'rxjs';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;  // Fixture in angular is like wrapper for our component which has additional functionality
+  let userService: UserService;
+  let userServiceSpy: jasmine.Spy;
 
   beforeEach(async () => {
-    TestBed.configureTestingModule({
-      declarations: [UserListComponent]    // in service testing we havn't define the declarations, but in component angular handles component, directive and pipe differntly in comparison to services
-    })
+
+    await TestBed.configureTestingModule({
+      declarations: [UserListComponent],    // in service testing we havn't define the declarations, but in component angular handles component, directive and pipe differntly in comparison to services
+      providers: [UserService]
+    }).compileComponents();
+
 
     fixture = TestBed.createComponent(UserListComponent);   // create a component of type UserListComponent
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    
+    userService = TestBed.inject(UserService);
+    userServiceSpy = spyOn(userService, 'getUsers').and.returnValue(of([      // chaining with the help of jasmine
+      { id: 1, name: "Akshat" },
+      { id: 2, name: "Shivam" }
+    ]))
+    // return mock data
 
     // why we need fixture? The component itself only gives us access to component properties and all of what's inside of the component but for our unit test we may be want to triggers some life cycle's or we want to trigger the change detection
   });
