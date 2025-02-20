154241ce523fa621288742b558f34b0711e7704b
akshatkumar <akshatk806@gmail.com>
2025-02-20 22:39:45 +0530

    component fixture for unit test

--- src/app/user/user-list/user-list.component.spec.ts
+++ src/app/user/user-list/user-list.component.spec.ts
@@ -4,17 +4,18 @@ import { UserListComponent } from './user-list.component';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
-  let fixture: ComponentFixture<UserListComponent>;
+  let fixture: ComponentFixture<UserListComponent>;  // Fixture in angular is like wrapper for our component which has additional functionality
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [UserListComponent]
+    TestBed.configureTestingModule({
+      declarations: [UserListComponent]    // in service testing we havn't define the declarations, but in component angular handles component, directive and pipe differntly in comparison to services
     })
-    .compileComponents();
 
-    fixture = TestBed.createComponent(UserListComponent);
+    fixture = TestBed.createComponent(UserListComponent);   // create a component of type UserListComponent
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    // why we need fixture? The component itself only gives us access to component properties and all of what's inside of the component but for our unit test we may be want to triggers some life cycle's or we want to trigger the change detection
   });
 
   it('should create', () => {
